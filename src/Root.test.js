d0a2e4a1a6afb3f97a21d89558559f5b5a20d0cc
Piotr Kawula <kontakt@piotrkawula.pl>
2020-10-01 12:42:27 +0200

    Test modal and if its rendered after clicking add button

--- src/Root.test.js
+++ src/Root.test.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import { cleanup, render } from '@testing-library/react';
+import { cleanup, fireEvent, render } from '@testing-library/react';
 import { theme } from 'theme/mainTheme';
 import { ThemeProvider } from 'styled-components';
 import Modal from 'components/Modal/Modal';
+import ButtonIcon from 'components/ButtonIcon/ButtonIcon';
 
 afterEach(cleanup);
 
 const renderModal = () => {
     const utils = render(
         <ThemeProvider theme={theme}>
-            <Modal toggleModal={() => ''} setIsModalOpened={() => ''} title="Modal testig">
-                Hello
+            <Modal toggleModal={() => ''} setIsModalOpened={() => ''} title="Title">
+                Modal
             </Modal>
         </ThemeProvider>
     );
@@ -20,10 +21,35 @@ const renderModal = () => {
     return { ...utils, modal };
 };
 
+const renderButton = () => {
+    const utils = render(
+        <ThemeProvider theme={theme}>
+            <ButtonIcon data-testid="open-modal-btn" />
+        </ThemeProvider>
+    );
+    const button = utils.getByTestId("open-modal-btn");
+
+    return { ...utils, button };
+
+}
+
 
 describe("Modal", () => {
     it("renders modal", () => {
         const { modal } = renderModal();
         expect(modal).toBeInTheDocument();
     });
+
+    it("renders modal when click button", () => {
+        const { button } = renderButton();
+
+        // check if button is rendered
+        expect(button).toBeInTheDocument;
+
+        // fire click event on button
+        fireEvent.click(button);
+        const { modal } = renderModal();
+        expect(modal).toBeInTheDocument();
+
+    })
 });
